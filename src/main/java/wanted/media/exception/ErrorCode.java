e6d651df4d7144dccdf1fc3de34e6172153e7cc2
pie <yejinh_h@naver.com>
2024-08-21 15:34:02 +0900

    feat : 에러 처리 및 에러 핸들링 설정

---
+++ src/main/java/wanted/media/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package wanted.media.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+	ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 엔티티입니다.");
+
+	private final HttpStatus status;
+	private final String message;
+}
