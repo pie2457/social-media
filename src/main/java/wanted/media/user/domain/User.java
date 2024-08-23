6461960f8626ec62935d667b3a38bac35afadeee
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-08-23 10:59:01 +0900

    refactor: User Entity user_id 컬럼명 수정

--- src/main/java/wanted/media/user/domain/User.java
+++ src/main/java/wanted/media/user/domain/User.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "user_Id", nullable = false)
+    @Column(name = "user_id", nullable = false)
     private UUID userId;
 
 }
