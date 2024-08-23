8a1cdda040f4e75b2b3a115cb8c43b9c04b73e21
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-08-23 09:31:14 +0900

    refactor: contents 기본 코드

--- src/main/java/wanted/media/content/domain/Content.java
+++ src/main/java/wanted/media/content/domain/Content.java
@@ -3,8 +3,10 @@ package wanted.media.content.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,33 +16,40 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
-@Table(name="contents")
+@Table(name = "contents")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ToString
 @EntityListeners(AuditingEntityListener.class)
 public class Content {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "content_id", nullable = false)
     private Long id;
+
     private Long likeCount;
+
     @Size(max = 50)
-    private String type;
+    private Enum type;
+
     @Size(max = 150)
     private String title;
+
     private String content;
+
     private String hashtags;
+
     private Long viewCount;
+
     private Long shareCount;
+
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
     @CreatedDate
     private LocalDateTime createdAt;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     @NotNull
     private User user;
