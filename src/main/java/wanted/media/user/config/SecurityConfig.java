2d8e04e8d765f4f5828123201849f87d3a8e910d
pie <yejinh_h@naver.com>
2024-08-25 17:11:42 +0900

    feat : api 테스트를 위한 security 임의 설정

--- src/main/java/wanted/media/user/config/SecurityConfig.java
+++ src/main/java/wanted/media/user/config/SecurityConfig.java
@@ -1,7 +1,27 @@
 package wanted.media.user.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+    // 해당 파일은 api 테스트하느라 임의로 추가해놓은 메서드입니다!
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/api/statistics/**").permitAll()
+                    .anyRequest().authenticated()  // 그 외의 경로는 인증 필요
+            )
+            .anonymous(Customizer.withDefaults())
+            .build();
+    }
 }
