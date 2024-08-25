97059e41e86725354e013f6ac448b6c037959de6
jeongeungyeong <jeongeung1124@gmail.com>
2024-08-25 16:23:46 +0900

    feat : users SecurityConfig 비밀번호 암호화 기능 추가

--- src/main/java/wanted/media/user/config/SecurityConfig.java
+++ src/main/java/wanted/media/user/config/SecurityConfig.java
@@ -1,7 +1,16 @@
 package wanted.media.user.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+    // 비밀번호 암호화 기능
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
