41cd12ec4dab8a80509cd2c93853e1c2c5ee0a87
pie <yejinh_h@naver.com>
2024-08-25 16:28:23 +0900

    refactor : Post 엔티티 수정
    - Content -> Post로 클래스 네임 변경
    - PK : Long -> String 으로 타입 변경
    - ContentController -> PostController 클래스 네임 변경

---
+++ src/main/java/wanted/media/content/domain/Post.java
@@ -0,0 +1,67 @@
+package wanted.media.content.domain;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import wanted.media.user.domain.User;
+
+@Entity
+@Getter
+@Table(name = "post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id", nullable = false)
+	private String id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Type type;
+
+	@Size(max = 150)
+	@Column(nullable = false)
+	private String title;
+	private String content;
+	private String hashtags;
+
+	@ColumnDefault("0")
+	private Long likeCount;
+
+	@ColumnDefault("0")
+	private Long viewCount;
+
+	@ColumnDefault("0")
+	private Long shareCount;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+}
