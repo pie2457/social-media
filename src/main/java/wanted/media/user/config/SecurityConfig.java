5c6d9b842a13d9edcb30d491c710d54c037ce002
jeongeungyeong <jeongeung1124@gmail.com>
2024-08-25 22:19:19 +0900

    feat : api 테스트를 위한 security 임시 설정

--- src/main/java/wanted/media/user/config/SecurityConfig.java
+++ src/main/java/wanted/media/user/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package wanted.media.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -12,5 +14,20 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // 임시 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll()  // 모든 요청 허용
+                )
+                .csrf().disable()  // CSRF 보호 비활성화 (테스트 목적으로만 사용)
+                .formLogin().disable()  // 로그인 폼 비활성화
+                .httpBasic().disable();  // HTTP Basic 인증 비활성화
+
+        return http.build();
     }
 }
