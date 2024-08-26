cee297593403c54f743bf24cfbea07eb30550431
Jinhui <jda0526@naver.com>
2024-08-26 12:44:03 +0900

    feat : PostIdResponse를 사용하여 응답 형식 구조 개선

--- src/main/java/wanted/media/post/controller/PostController.java
+++ src/main/java/wanted/media/post/controller/PostController.java
@@ -6,16 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wanted.media.post.dto.PostIdResponse;
 import wanted.media.post.service.PostService;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
-
     private final PostService postService;
 
     @PostMapping("/likes/{postId}")
@@ -26,10 +23,7 @@ public class PostController {
 
         try {
             String id = postService.increaseLike(postId);
-            Map<String, Object> response = new HashMap<>();
-            response.put("postId", id);
-            response.put("message", "좋아요 수 증가 완료");
-            return ResponseEntity.ok().body(response);
+            return ResponseEntity.ok().body(new PostIdResponse(id, "좋아요 수 증가 완료"));
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body("좋아요 수 증가 실패 : " + e.getMessage());
         }
---
+++ src/main/java/wanted/media/post/dto/PostIdResponse.java
@@ -0,0 +1,11 @@
+package wanted.media.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostIdResponse {
+    private final String postId;
+    private final String messgae;
+}
