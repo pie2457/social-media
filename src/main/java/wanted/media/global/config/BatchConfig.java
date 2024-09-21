57745dea068c8a52b09db582505833c1c9dd2a15
pie2457 <yejinh_h@naver.com>
2024-09-21 22:17:56 +0900

    feat: [#1] batch config 작성

---
+++ src/main/java/wanted/media/global/config/BatchConfig.java
@@ -0,0 +1,12 @@
+package wanted.media.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+}
